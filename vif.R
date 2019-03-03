403f5d11f680a4780daad4742bfcfc21fd733d48
Mike Sankari <msankari@outlook.com>
2019-03-03 12:10:15 -0500

    added ridfe and lasso amd vif

--- vif.R
+++ vif.R
@@ -1,1 +1,39 @@
+library(car)
 setwd('/Users/michaelsankari/Documents/NYC Data Science/Machine Learning Project/github/ML-project-for-NYCDSA')
+df <- read.csv('./house-prices-advanced-regression-techniques/cleandata/s2_clean_dummified.csv')
+
+#Remove these columns, ID is not releveant, the other 3 are not in test data
+drops <- c('Exterior1st_ImStucc', 'Exterior1st_Stone', 'HouseStyle_2.5Fin', 'Id')
+df <- df[, !(names(df) %in% drops)]
+
+fit <- lm(LogSalePrice~., data = df)
+summary(fit)
+vif(fit) #creates area because one or more variable is linearly dependent on the others
+
+alias(lm(LogSalePrice~., data = df)) #Problem is BsmtCond_None
+df$BsmtCond_None <- NULL
+
+fit <- lm(LogSalePrice~., data = df)
+summary(fit)
+
+#Do vif and put into data frame that is easier to read
+vif_fit <- data.frame(vif(fit))
+vif_fit <- data.frame(variable = row.names(vif_fit), vif_score=vif_fit$vif.fit.)
+vif_fit$variable <- as.character(vif_fit$variable)
+vif_fit <- vif_fit[order(vif_fit$vif_score, decreasing = TRUE),]
+row.names(vif_fit) <- 1:nrow(vif_fit)
+
+#Which ones are less than ...
+keep <- vif_fit$variable[vif_fit$vif_score<3]
+keep <- c(keep, 'LogSalePrice')
+
+df <- df[, (names(df) %in% keep)]
+
+fit2 <- lm(LogSalePrice~., data = df)
+summary(fit2)
+vif(fit2)
+
+AIC(fit, fit2)
+anova(fit2, fit)
+
+write.csv(vif_fit, file = 'vif_scores.csv', row.names = FALSE)
--- vif.py
+++ vif.py
@@ -19,35 +19,64 @@ plt.style.use('ggplot')
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
 import datetime
+import pickle
 
-def linear(X, Y, vif):
-    #running ridge with alpha 0 (MLR)
-    linear = LinearRegression()
-    linear.fit(X, Y)
-   
-    raw_test = pd.read_csv(".//house-prices-advanced-regression-techniques/cleandata/s2_clean_dummified_test.csv")
-    test_IDs = raw_test['Id']
-    test_IDs = pd.DataFrame(test_IDs)
-    raw_test.drop(['Id', 'Exterior1st_Other'], axis = 1, inplace = True)
-    raw_test.drop(vif, axis=1, inplace=True)
+def get_stack(X, Y):
+        
+    ridge_file = 'vif ridge2019-02-28 18/10/09.790796.sav'
+    ridge_file = ridge_file.replace('/', ':') #python is looking for colons, not slashes
+    ridge = pickle.load(open(ridge_file, 'rb'))
+    
+    linear_file = 'vif linear2019-02-28 18/10/09.871399.sav'
+    linear_file = linear_file.replace('/', ':') #python is looking for colons, not slashes
+    linear = pickle.load(open(linear_file, 'rb'))
+    
+    final_model = LinearRegression()
+    
+    final_model.intercept_ = (ridge.intercept_ + linear.intercept_)/2
+    final_model.coef_ = (ridge.coef_ + linear.coef_)/2
+    
+    final_model.predict(X)
+    final_model.score(X, Y)
     
     suffix = str(datetime.datetime.now())
-    filename = 'vif linear ' + suffix 
+    model_filename = 'vif stack' + suffix +'.sav'
+    pickle.dump(linear, open(model_filename, 'wb'))
+    csv_filename = 'vif stack ' + suffix + '.csv'
+    
+    raw_test, test_IDs = load_test()
+    predict = final_model.predict(raw_test)
+    predict = np.exp(predict)
+    predict = pd.DataFrame(predict)
+    predict = pd.concat([test_IDs, predict], axis = 1)
+    predict.columns = ['Id', 'SalePrice']
+    predict.to_csv(csv_filename, index=False)
+
+def get_linear(X, Y, vif):
+    linear = LinearRegression()
+    linear.fit(X, Y)
+    
+    suffix = str(datetime.datetime.now())
+    model_filename = 'vif linear' + suffix +'.sav'
+    pickle.dump(linear, open(model_filename, 'wb'))
+    csv_filename = 'vif linear ' + suffix + '.csv'
+    
+    raw_test, test_IDs = load_test()
     predict = linear.predict(raw_test)
     predict = np.exp(predict)
     predict = pd.DataFrame(predict)
     predict = pd.concat([test_IDs, predict], axis = 1)
     predict.columns = ['Id', 'SalePrice']
-    predict.to_csv(filename, index=False)
+    predict.to_csv(csv_filename, index=False)
 
-def ridge(X, Y, vif):
+def get_ridge(X, Y, vif):
     #running ridge with alpha 0 (MLR)
     ridge = Ridge()
     ridge.set_params(alpha = 0, normalize = True)
     ridge.fit(X, Y)
     
     #Grid search for ridge
-    alphas_ridge = np.linspace(0,10,50)
+    alphas_ridge = np.linspace(0,15,50)
     tuned_parameters_r = [{'alpha': alphas_ridge}]
     n_folds = 5
     cv = KFold(n_splits=n_folds, shuffle=True)
@@ -65,23 +94,21 @@ def ridge(X, Y, vif):
     ridge_best.fit(X,Y)
     print(ridge_best.score(X,Y))
     
-    raw_test = pd.read_csv(".//house-prices-advanced-regression-techniques/cleandata/s2_clean_dummified_test.csv")
-    test_IDs = raw_test['Id']
-    test_IDs = pd.DataFrame(test_IDs)
-    raw_test.drop(['Id', 'Exterior1st_Other'], axis = 1, inplace = True)
-    raw_test.drop(vif, axis=1, inplace=True)
-    
     suffix = str(datetime.datetime.now())
-    filename = 'vif ridge ' + suffix 
-    predict_ridge = ridge_best.predict(raw_test)
-    predict_ridge = np.exp(predict_ridge)
-    predict_ridge = pd.DataFrame(predict_ridge)
-    predict_ridge = pd.concat([test_IDs, predict_ridge], axis = 1)
-    predict_ridge.columns = ['Id', 'SalePrice']
-    predict_ridge.to_csv(filename, index=False)
+    model_filename = 'vif ridge' + suffix +'.sav'
+    pickle.dump(ridge_best, open(model_filename, 'wb'))
+    csv_filename = 'vif ridge ' + suffix + '.csv'
+    
+    raw_test, test_IDs = load_test()
+    predict = ridge_best.predict(raw_test)
+    predict = np.exp(predict)
+    predict = pd.DataFrame(predict)
+    predict = pd.concat([test_IDs, predict], axis = 1)
+    predict.columns = ['Id', 'SalePrice']
+    predict.to_csv(csv_filename, index=False)
 
 
-def lasso(X, Y, vif):
+def get_lasso(X, Y, vif):
     #running ridge with alpha 0 (MLR)
     lasso = Lasso()
     lasso.set_params(alpha = 0, normalize = True)
@@ -105,6 +132,45 @@ def lasso(X, Y, vif):
     lasso_best = tune_lasso.best_estimator_
     lasso_best.fit(X,Y)
     print(lasso_best.score(X,Y))
+
+    suffix = str(datetime.datetime.now())
+    model_filename = 'vif lasso' + suffix +'.sav'
+    pickle.dump(lasso_best, open(model_filename, 'wb'))
+    csv_filename = 'vif ridge ' + suffix + '.csv'
+    
+    raw_test, test_IDs = load_test()
+    predict = lasso_best.predict(raw_test)
+    predict = np.exp(predict)
+    predict = pd.DataFrame(predict)
+    predict = pd.concat([test_IDs, predict], axis = 1)
+    predict.columns = ['Id', 'SalePrice']
+    predict.to_csv(csv_filename, index=False)
+
+def load_train():
+    '''
+    Reads in the dummified data set. Does further cleaning to drop unnecessary columns
+    and does transformations on price.
+    Returns raw, the final data set for the features and sale_price, the final dependent variable
+    '''
+    os.chdir('/Users/michaelsankari/Documents/NYC Data Science/Machine Learning Project/github/ML-project-for-NYCDSA')
+    raw = pd.read_csv("./house-prices-advanced-regression-techniques/cleandata/s2_clean_dummified.csv")
+    
+    outliers = raw[ (raw['GrLivArea'] > 4000) & (raw['LogSalePrice'] < 13) ].index
+    raw.drop(outliers,axis=0, inplace=True)
+
+    sale_price = raw['LogSalePrice']
+    raw = raw.drop(['LogSalePrice','Id'],axis=1)
+    
+    #Remove columns from train that are not in test data rather than making them 0 in test
+    raw = raw.drop(['Exterior1st_ImStucc', 'Exterior1st_Stone', 'HouseStyle_2.5Fin'] ,axis=1)
+    
+    return(raw, sale_price)
+    
+def load_test():
+    '''
+    Reads in and cleans test data.
+    Returns a data frame of cleaned test data
+    '''
     
     raw_test = pd.read_csv(".//house-prices-advanced-regression-techniques/cleandata/s2_clean_dummified_test.csv")
     test_IDs = raw_test['Id']
@@ -112,31 +178,11 @@ def lasso(X, Y, vif):
     raw_test.drop(['Id', 'Exterior1st_Other'], axis = 1, inplace = True)
     raw_test.drop(vif, axis=1, inplace=True)
     
-    suffix = str(datetime.datetime.now())
-    filename = 'vif lasso ' + suffix 
-    predict = lasso_best.predict(raw_test)
-    predict = np.exp(predict)
-    predict = pd.DataFrame(predict)
-    predict = pd.concat([test_IDs, predict], axis = 1)
-    predict.columns = ['Id', 'SalePrice']
-    predict.to_csv(filename, index=False)
+    return(raw_test, test_IDs)
 
-os.chdir('/Users/michaelsankari/Documents/NYC Data Science/Machine Learning Project/github/ML-project-for-NYCDSA')
-raw = pd.read_csv("./house-prices-advanced-regression-techniques/cleandata/s2_clean_dummified.csv")
-raw.shape
 
 pd.set_option('display.max_columns', 150)
-raw.head()
-
-#remove outliers
-outliers = raw[ (raw['GrLivArea'] > 4000) & (raw['LogSalePrice'] < 13) ].index
-raw.drop(outliers,axis=0, inplace=True)
-
-sale_price = raw['LogSalePrice']
-raw = raw.drop(['LogSalePrice','Id'],axis=1)
-
-#Remove columns from train that are not in test data rather than making them 0 in test
-raw = raw.drop(['Exterior1st_ImStucc', 'Exterior1st_Stone', 'HouseStyle_2.5Fin'] ,axis=1)
+raw, sale_price = load_train()
 
 vif = pd.read_csv('vif_scores.csv')
 #Keep only those with score under ...
@@ -148,6 +194,8 @@ raw = raw.drop(vif, axis=1)
 X = raw.copy()
 Y = sale_price.copy()
 
-#ridge(X,Y, vif)
-#lasso(X, Y, vif)
-linear(X, Y, vif)
+#get_ridge(X,Y, vif)
+#get_lasso(X, Y, vif)
+#get_linear(X, Y, vif)
+
+get_stack(X, Y)
